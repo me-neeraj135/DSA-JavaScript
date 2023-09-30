7eb9d9adfb7b2c94c435839a35a2942b36398e7b
neeraj <me.neeraj1455@gmail.com>
2023-09-30 22:55:47 +0530

    check Power of two

---
+++ powerOfTwo.js
@@ -0,0 +1,19 @@
+// proble- Give a positive interger "n" determine if the number is power of two or not.
+
+// An interger is a power of two if there exists interger "x" such that "n"===2**2
+
+function isPowerOfTwo(n) {
+  if (n < 1) {
+    return false;
+  }
+  while (n > 1) {
+    if (n % 2 !== 0) {
+      return false;
+    }
+    n = n / 2;
+  }
+  return true;
+}
+console.log(isPowerOfTwo(1));
+console.log(isPowerOfTwo(2));
+console.log(isPowerOfTwo(5));
