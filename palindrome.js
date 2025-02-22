46cda36f5c12abb4a6ad8369935821a4da05cf1b
Neeraj Kumar <me.neeraj1455@gmail.com>
2025-02-22 15:47:57 +0530

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -3,17 +3,12 @@
 // A string is a palindrome if it is read the same from forward or backward. For example, dad reads the same either from forward or backward. So the word dad is a palindrome. Similarly, madam is also a palindrome.
 
 function checkPal(str) {
-  let newStr = str.toLowerCase();
-  //   console.log(newStr, `newString`);
-  let left = 0;
-  let right = newStr.length - 1;
-  while (left < right) {
-    if (newStr[left] !== newStr[right]) return false;
-    left++;
-    right--;
+  str = str.toLowerCase();
+  for (let i = 0, j = str.length - 1; i < j; i++, j--) {
+    if (str[i] !== str[j]) return false;
   }
   return true;
 }
-let res = checkPal(`madam`);
 
-console.log(res);
+console.log(checkPal("madam"));
+
